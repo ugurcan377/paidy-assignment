ed8c3b2ae46488bdd010f57040ec9b949fc2a858
Ugurcan Ergun <ugurcanergn@gmail.com>
2024-08-26 12:45:34 +0900

    Add tests for the API

--- src/main.rs
+++ src/main.rs
@@ -165,3 +165,372 @@ async fn delete_order(Path(id): Path<i32>, State(pool): State<PgPool>,) -> impl 
     }
     return StatusCode::NOT_FOUND
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{self, Request, StatusCode},
+    };
+    use http_body_util::BodyExt; // for `collect`
+    use serde_json::{json, Value};
+    use tower::{Service, ServiceExt}; // for `call`, `oneshot`, and `ready`
+
+    #[sqlx::test]
+    async fn test_root(pool: PgPool) {
+        let app = app(pool).await;
+
+        let response = app
+            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        assert_eq!(&body[..], b"Paidy Restaurant API");
+    }
+
+    #[sqlx::test(fixtures("orders"))]
+    async fn test_create_tables(pool: PgPool) {
+        let app = app(pool).await;
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::POST)
+                    .uri("/tables/5")
+                    .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                    .body(Body::from(
+                        serde_json::to_vec(&json!({
+                            "orders": [{"item_name": "pasta", "duration": 14}]
+                        })).unwrap()
+                    ))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body["orders"].as_array().unwrap().len(), 1);
+    }
+
+    #[sqlx::test(fixtures("orders"))]
+    async fn test_create_multiple_tables(pool: PgPool) {
+        let app = app(pool).await;
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::POST)
+                    .uri("/tables/5")
+                    .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                    .body(Body::from(
+                        serde_json::to_vec(&json!({
+                            "orders": [{"item_name": "pasta"}, {"item_name": "doria"}]
+                        })).unwrap()
+                    ))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body["orders"].as_array().unwrap().len(), 2);
+    }
+
+    #[sqlx::test(fixtures("orders"))]
+    async fn test_create_multiple_tables_with_empty_items(pool: PgPool) {
+        let app = app(pool).await;
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::POST)
+                    .uri("/tables/5")
+                    .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                    .body(Body::from(
+                        serde_json::to_vec(&json!({
+                            "orders": [{"item_name": "pasta"}, {}]
+                        })).unwrap()
+                    ))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body["orders"].as_array().unwrap().len(), 1);
+    }
+
+    #[sqlx::test(fixtures("orders"))]
+    async fn test_add_orders_to_table(pool: PgPool) {
+        let app = app(pool).await;
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::POST)
+                    .uri("/tables/14")
+                    .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                    .body(Body::from(
+                        serde_json::to_vec(&json!({
+                            "orders": [{"item_name": "pasta"}, {"item_name": "doria"}]
+                        })).unwrap()
+                    ))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body["orders"].as_array().unwrap().len(), 4);
+    }
+
+    #[sqlx::test(fixtures("orders"))]
+    async fn test_create_empty_tables(pool: PgPool) {
+        let app = app(pool).await;
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::POST)
+                    .uri("/tables/5")
+                    .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                    .body(Body::from(
+                        serde_json::to_vec(&json!({
+                            "orders": []
+                        })).unwrap()
+                    ))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body["orders"].as_array().unwrap().len(), 0);
+    }
+
+    #[sqlx::test(fixtures("orders"))]
+    async fn test_create_tables_wrong_type(pool: PgPool) {
+        let app = app(pool).await;
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::POST)
+                    .uri("/tables/5")
+                    .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                    .body(Body::from(
+                        serde_json::to_vec(&json!({
+                            "orders": [{"item_name": 15}]
+                        })).unwrap()
+                    ))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::UNPROCESSABLE_ENTITY);
+    }
+
+    #[sqlx::test(fixtures("orders"))]
+    async fn test_create_tables_empty_body(pool: PgPool) {
+        let app = app(pool).await;
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::POST)
+                    .uri("/tables/5")
+                    .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+    }
+
+    #[sqlx::test(fixtures("orders"))]
+    async fn test_list_tables(pool: PgPool) {
+        let app = app(pool).await;
+        let response = app
+        .oneshot(Request::builder().uri("/tables").body(Body::empty()).unwrap())
+        .await
+        .unwrap();
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body, json!({
+            "orders": [
+                {
+                    "id": 1,
+                    "table_no": 14,
+                    "item_name": "karaage",
+                    "duration": 10
+                },
+                {
+                    "id": 2,
+                    "table_no": 15,
+                    "item_name": "yakisoba",
+                    "duration": 7
+                },
+                {
+                    "id": 3,
+                    "table_no": 14,
+                    "item_name": "takoyaki",
+                    "duration": 6
+                },
+            ] 
+        }));
+    }
+
+    #[sqlx::test(fixtures("orders"))]
+    async fn test_get_tables(pool: PgPool) {
+        let app = app(pool).await;
+        let response = app
+        .oneshot(Request::builder().uri("/tables/14").body(Body::empty()).unwrap())
+        .await
+        .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body, json!({
+            "orders": [
+                {
+                    "id": 1,
+                    "table_no": 14,
+                    "item_name": "karaage",
+                    "duration": 10
+                },
+                {
+                    "id": 3,
+                    "table_no": 14,
+                    "item_name": "takoyaki",
+                    "duration": 6
+                },
+            ] 
+        }));
+    }
+
+    #[sqlx::test(fixtures("orders"))]
+    async fn test_get_order(pool: PgPool) {
+        let app = app(pool).await;
+        let response = app
+        .oneshot(Request::builder().uri("/orders/3").body(Body::empty()).unwrap())
+        .await
+        .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body, json!({
+            "id": 3,
+            "table_no": 14,
+            "item_name": "takoyaki",
+            "duration": 6
+        }));
+    }
+
+    #[sqlx::test(fixtures("orders"))]
+    async fn test_get_non_existant_order(pool: PgPool) {
+        let app = app(pool).await;
+        let response = app
+        .oneshot(Request::builder().uri("/orders/99").body(Body::empty()).unwrap())
+        .await
+        .unwrap();
+
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+    }
+
+    #[sqlx::test(fixtures("orders"))]
+    async fn test_delete_table(pool: PgPool) {
+        let app = app(pool).await;
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::DELETE)
+                    .uri("/tables/14")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::NO_CONTENT);
+    }
+
+    #[sqlx::test(fixtures("orders"))]
+    async fn test_delete_non_existing_table(pool: PgPool) {
+        let app = app(pool).await;
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::DELETE)
+                    .uri("/tables/999")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+    }
+
+    #[sqlx::test(fixtures("orders"))]
+    async fn test_delete_order(pool: PgPool) {
+        let app = app(pool).await;
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::DELETE)
+                    .uri("/orders/3")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::NO_CONTENT);
+    }
+
+    #[sqlx::test(fixtures("orders"))]
+    async fn test_delete_non_existing_order(pool: PgPool) {
+        let app = app(pool).await;
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::DELETE)
+                    .uri("/orders/999")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+    }
+}
